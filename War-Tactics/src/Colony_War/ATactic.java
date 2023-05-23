97ad6776454a1df41a128d322d7d67e23990881f
MAHI-HAKIM <103943510+MAHI-HAKIM@users.noreply.github.com>
2023-05-23 10:39:56 +0300

    Implemted A war tactic

--- War-Tactics/src/Colony_War/ATactic.java
+++ War-Tactics/src/Colony_War/ATactic.java
@@ -1,9 +1,18 @@
 package Colony_War;
+import java.util.Random;
 
-public class ATactic {
+public class ATactic extends Tactic {
 
-	public ATactic() {
-		// TODO Auto-generated constructor stub
+	public ATactic() {	
 	}
+	
+	@Override
+    public int War() {
+		Random random = new Random();
+		int random1 = random.nextInt(500)+1; // Generate random number between 1 to 500
+		int random2 = random.nextInt(500)+501; // Generate random number between 500 to 501
+		int strength = (random.nextInt(2)==0) ? random1 : random2; // Choose one random from either 1 or 2
+        return strength;
+    }
 
 }
