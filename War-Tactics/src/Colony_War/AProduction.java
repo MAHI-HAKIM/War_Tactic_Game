c9e98d297e034abc4bedc3162fb4850b1b69f858
MAHI-HAKIM <103943510+MAHI-HAKIM@users.noreply.github.com>
2023-05-23 10:48:57 +0300

    Implemented A production

--- War-Tactics/src/Colony_War/AProduction.java
+++ War-Tactics/src/Colony_War/AProduction.java
@@ -1,9 +1,27 @@
 package Colony_War;
+import java.util.Random;
 
-public class AProduction {
+
+public class AProduction extends Production {
 
 	public AProduction() {
 		// TODO Auto-generated constructor stub
 	}
 
+	@Override
+	public int Produce()
+	{
+		Random random = new Random();
+		
+		int rand_num = random.nextInt(10) +1 ; //Generate Random number from 1 to 10
+		
+		if(rand_num < 5)
+		{
+			return (rand_num + 2)%11; // to ensure that the number stays with in the range of 1-10
+		}
+		else
+		{
+			return rand_num;
+		}
+	}
 }
